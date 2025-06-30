483a63327fd15692408c418a5124244f18935a96
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-06-30 15:05:22 -0400

    customers page implemented

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from "next";
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { Suspense } from 'react';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers, fetchInvoicesPages } from "@/app/lib/data";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import Pagination from "@/app/ui/invoices/pagination";
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+  const customers = await fetchFilteredCustomers(query)
+
+
+  return (
+    <div className="w-full">
+
+      {<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>}
+      <div className="mt-5 flex w-full justify-center">
+        {<Pagination totalPages={totalPages} />}
+      </div>
+    </div>
+  )
 }
