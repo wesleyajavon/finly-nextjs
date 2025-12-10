92324f7e2038770677fca70cc784042dd43c4e06
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-12-10 04:15:10 -0500

    Fix next.config lint option and build

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -5,11 +5,6 @@ const nextConfig: NextConfig = {
   //   experimental: {
   //   ppr: 'incremental'
   // }
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
